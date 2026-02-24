2c2d8fdb4018d0802dccf98802eea3853851f6a0
Mohannad abo alfadel <133568307+MohannadAboAlfadel@users.noreply.github.com>
2026-02-24 12:20:01 +0300

    style(current-weather): improve styling and component structure
    - Adjust box shadow opacity and add background color for better contrast
    - Add margin, padding, and text color for improved layout
    - Wrap city and description in a div for better alignment
    - Fix formatting and indentation in JSX and CSS

--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -1,16 +1,17 @@
-import './current-weather.css';
+import "./current-weather.css";
 
-const CurrentWeather =() =>{
-return (
-    <div className ="weather"> 
-     <div className ="top"> 
-        <p className="city">City</p> 
-        <p className="weather-descripstion">Temp</p> 
-     </div>
-     <img alt ="weather" className ="weather-icon" src="icons/01d.png"/> 
-
+const CurrentWeather = () => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">City</p>
+          <p className="weather-descripstion">Temp</p>
+        </div>
+      </div>
+      <img alt="weather" className="weather-icon" src="icons/01d.png" />
     </div>
-);
-}
+  );
+};  
 
 export default CurrentWeather;
